2a881c95d4b46f42f646a2947494932dcb463348
Landcruiser87 <andyheroy@gmail.com>
2020-12-18 21:03:53 -0500

    Stupid vscode folders always sneak through

--- scripts/start.py
+++ scripts/start.py
@@ -1,8 +1,22 @@
+#%%
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.svm import  SVC
+from sklearn.decomposition import TruncatedSVD
 
-movies = pd.read_csv('..\data\movies.csv', header = 0)
-ratings = pd.read_csv('..\data\ratings.csv', header = 0)
+mov_df = pd.read_csv("..\data\movies.csv", header = 0)
+rate_df = pd.read_csv("..\data\mov_ratings.csv", header = 0)
+
+#Poking around the movie df
+mov_df.describe()
+mov_df.columns
+mov_df.isnull().sum()
+
+
+rate_df.describe()
+rate_df.columns
+rate_df.isnull().sum()
+#Yay no nulls
+
 
